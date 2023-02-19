576a70d8e167183a89ed0174df1e759baee71f2e
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-02-19 23:52:51 +0100

    perf(store.js): implement `@redux/toolkit`

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./index.css";
+import { Counter } from "./components/Counter";
 
 function App() {
   return (
     <div className="m-0 p-0">
       <h1 className="text-3xl font-bold underline ">Galaxy</h1>
+      <Counter />
     </div>
   );
 }
