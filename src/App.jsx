d8055888ef9d99b4a3bd6321c6b8d55633ad77a8
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-02-19 22:28:50 +0100

    fix(package.json): delete type module from package

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import './App.css'
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-       Galaxy
-      </h1>
-
+    <div className="m-0 p-0">
+      <h1 className="text-3xl font-bold underline ">Galaxy</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: false,
-  },
   test: {
     css: false,
-    include: ['src/**/__tests__/*'],
+    include: ["src/**/__tests__/*"],
     globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "src/setupTests.ts",
     clearMocks: true,
   },
 });
